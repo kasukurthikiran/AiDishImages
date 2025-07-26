26c1e17d082c554bfe51c6fe2304d216e9dce606
kasukurthikiran <sk@konic.net>
2025-07-26 11:14:05 +0530

    feth:used post resuest using curl

--- backend_fast_api/items.py
+++ backend_fast_api/items.py
@@ -1,32 +1,6 @@
-# # routers/items.py
-# from fastapi import APIRouter
-
-# router = APIRouter()
-
-
-# @router.get("/items")
-# async def get_items():
-#     return {"message": "List of items"}
-
-
-# @router.get("/items/{item_id}")
-# async def get_item(item_id: int):
-#     return {"item_id": item_id}
-
-
-# @router.get("/items/popular")
-# async def get_popular_items():
-#     return {"message": "Popular items list"}
-
-
-# @router.get("/items/by-name/{name}")
-# async def get_item_by_name(name: str):
-#     return {"message": f"Item with name {name}"}
-
-
 from fastapi import APIRouter
-from database_configaration import database_connection
-from database_configaration.models import Dishes, Restaurant
+from ..database_configaration.models import Dishes, Restaurant
+from ..database_configaration.database_connection import get_session
 
 from contextlib import contextmanager
 
@@ -37,7 +11,7 @@ router = APIRouter()
 
 @contextmanager
 def get_db_session():
-    yield from database_connection.get_session()
+    yield from get_session()
 
 
 class Dish(BaseModel):
