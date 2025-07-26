353d1866b634a3a19ae382519d19c6ce5aac5151
kasukurthikiran <sk@konic.net>
2025-07-26 11:14:05 +0530

    feth:used post resuest using curl

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 # main.py
 from fastapi import FastAPI  # Import the router
-from .backend_fast_api.routers import items
+from .backend_fast_api import items
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Include the router
 # app.include_router(items.router)
-app.include_router(items.router, prefix="/items")
+app.include_router(items.router)
