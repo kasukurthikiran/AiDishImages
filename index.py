9ab3ee6d6a9211bb470d0450863aab0fced8fd9e
kasukurthikiran <sk@konic.net>
2025-07-31 15:09:41 +0530

    chore: removed un used code in index.py

--- index.py
+++ index.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 from .backend_fast_api import router
 from fastapi.middleware.cors import CORSMiddleware
 
-# from pydantic import BaseModel
-from fastapi import APIRouter, UploadFile, File, Form
-import base64
+4
 
 app = FastAPI()
 
@@ -16,33 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/api/users")
-def get_users():
-    print("i am get users")
-    return [
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-        {"id": 3, "name": "Charlie"},
-    ]
-
-
-list1 = [
-    {"id": 1, "name": "Alice"},
-    {"id": 2, "name": "Bob"},
-    {"id": 3, "name": "Charlie"},
-]
-
-
-# @app.post("/upload/")
-# async def upload_restaurant(
-#     file: UploadFile = File(...),
-#     restaurant_id: int = Form(...),
-#     restaurant_name: str = Form(...),
-# ):
-# # Example: just return the name back
-# return {"details": list1}
 
 
 app.include_router(router.router)
